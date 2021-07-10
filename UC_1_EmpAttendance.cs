08ca54e30a7b27f32bbb484afd270aed878269b5
BootCampBL <satyabhandari43@gmail.com>
2021-07-10 17:20:02 +0530

    [sathya] Added- checks whether emp is absent or present

---
+++ UC_1_EmpAttendance.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class UC_1_EmpAttendance
+    {
+        public static void EmpAttendance()
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int result = random.Next(0, 2);
+            if (result == IS_FULL_TIME)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is absent");
+            Console.ReadKey();
+        }
+    }
+}
